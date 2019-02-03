5e2cfbadb36fa4f4f9ade317317326e1d35ea7d2
Ryan Wolfe <ryan@studiohopfitness.com>
2019-02-03 21:52:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,9 @@ def second_challenge
   }
 
   #code your solution here!
-groceries.values
-
-
+items = groceries[:dairy].values
+items << groceries[:vegetable].values
+items << groceries[:meat].values
+items << groceries[:grains].values
+items
 end
